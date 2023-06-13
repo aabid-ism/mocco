12200801020ae6662304c8cd3f141c4ec57072f4
Shrenik Deep <s.shrenikdeep@gmail.com>
2023-06-13 21:58:52 +0530

    POST Request Update

--- server/routes/news.js
+++ server/routes/news.js
@@ -3,20 +3,32 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/",  async (req, res) => {
-    try {
-            // getting references to database and collection
-            const db = conn.getDb();
-            const collection = await db.collection("news");
-            
-            // finding and returning all news posts
-            const results = await collection.find({}).limit(50).toArray();
-            res.send(results).status(200);
-    } 
-    catch (error) {
-        res.send(error).status(500);
-    }
-    });
+router.get("/", async (req, res) => {
+  try {
+    // getting references to database and collection
+    const db = conn.getDb();
+    const collection = await db.collection("news");
 
+    // finding and returning all news posts
+    const results = await collection.find({}).limit(50).toArray();
+    res.send(results).status(200);
+  } catch (error) {
+    res.send(error).status(500);
+  }
+});
+
+router.post("/add-news", async (req, res) => {
+  try {
+    const db = conn.getDb();
+    const collection = await db.collection("news");
+    let data = req.body;
+
+    const results = await collection.insertOne(data);
+    res.send(results).status(200);
+  } catch (error) {
+    console.error(error);
+    res.send(error).status(500);
+  }
+});
 
 export default router;

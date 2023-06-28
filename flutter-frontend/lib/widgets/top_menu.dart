0c1f0e1d746def042a72c7ef6f995a89bc3f177e
sathsarabandaraj <sathsarabjayasundara@gmail.com>
2023-06-28 23:25:33 +0530

    Improve top menu separator ui/ux

--- flutter-frontend/lib/widgets/top_menu.dart
+++ flutter-frontend/lib/widgets/top_menu.dart
@@ -16,32 +16,14 @@ class TopMenu extends StatelessWidget {
             text: 'Explorer',
             isActive: currentPageIndex == 0,
           ),
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 8),
-            child: Text(
-              "|",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 35  ,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
+          _TopMenuSeparator(
+              isActive: currentPageIndex == 0 || currentPageIndex == 1),
           _TopMenuText(
             text: 'News',
             isActive: currentPageIndex == 1,
           ),
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 8),
-            child: Text(
-              "|",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 35  ,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
+          _TopMenuSeparator(
+              isActive: currentPageIndex == 1 || currentPageIndex == 2),
           _TopMenuText(
             text: 'Lifestyle',
             isActive: currentPageIndex == 2,
@@ -66,6 +48,28 @@ class _TopMenuText extends StatelessWidget {
             isActive ? Colors.white : const Color.fromARGB(255, 209, 209, 209),
         fontSize: 20,
         fontWeight: isActive ? FontWeight.bold : FontWeight.w500,
+      ),
+    );
+  }
+}
+
+class _TopMenuSeparator extends StatelessWidget {
+  final bool isActive;
+  const _TopMenuSeparator({required this.isActive});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 8),
+      child: Text(
+        "|",
+        style: TextStyle(
+          color: isActive
+              ? Colors.white
+              : const Color.fromARGB(255, 209, 209, 209),
+          fontSize: 35,
+          fontWeight: isActive ? FontWeight.bold : FontWeight.normal,
+        ),
       ),
     );
   }

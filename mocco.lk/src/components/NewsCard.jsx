b2e131f55193518c6d0ceb8124d81b52883ede31
Aabid Ismail <aabidismail@Aabids-MacBook-Pro.local>
2023-07-04 12:44:30 +0530

    loading more posts functionality added

--- mocco.lk/src/components/NewsCard.jsx
+++ mocco.lk/src/components/NewsCard.jsx
@@ -2,14 +2,14 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import "./NewsCard.css"; // Import the CSS file
 
-function NewsCard({ heading, description }) {
+function NewsCard({ heading, description, imageUrl }) {
   return (
     <div>
       <Card
         className="custom-card"
         style={{ borderRadius: "10px", margin: "25px" }}
       >
-        <Card.Img variant="top" src="books.jpg" />
+        <Card.Img variant="top" src={imageUrl} />
         <Card.Body>
           <Card.Title>{heading}</Card.Title>
           <Card.Text>{description}</Card.Text>

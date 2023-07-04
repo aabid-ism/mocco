5de14105bf857a258f42bf3d1c0f9c69e814832f
Aabid Ismail <aabidismail@Aabids-MacBook-Pro.local>
2023-07-04 12:44:05 +0530

    fixed Feed endpoint, renamed erroneous created_at to createdAt

--- server/routes/news.js
+++ server/routes/news.js
@@ -39,8 +39,18 @@ router.get("/feed", async (req, res) => {
     const collection = await db.collection("news");
 
     // finding and returning all news posts
-    const results = await collection.find({}).limit(20).toArray();
-    results.reverse();
+    // const results = await collection.find({}).limit(2).toArray();
+
+    const results = await collection
+      .aggregate([
+        {
+          $sort: { createdAt: -1 },
+        },
+        {
+          $limit: 2,
+        },
+      ])
+      .toArray();
     res.send(results).status(200);
   } catch (error) {
     res.send(error).status(500);

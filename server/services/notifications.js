d2a1061bdc4a45e773de849f2090f42adfa8e481
Aabid Ismail <aabidismail@Aabids-MacBook-Pro.local>
2023-07-08 13:42:27 +0530

    notification service enclosed data object keys with paranthesis, included Basic infront of authorization header. Gets a 200 OK response for notifications

--- server/services/notifications.js
+++ server/services/notifications.js
@@ -6,23 +6,23 @@ async function sendNotification(postIndex) {
     url: "https://onesignal.com/api/v1/notifications",
     headers: {
       accept: "application/json",
-      Authorization: `${process.env.ONE_SIGNAL_REST_API_KEY}`,
+      Authorization: `Basic ${process.env.ONE_SIGNAL_REST_API_KEY}`,
       "content-type": "application/json",
     },
     data: {
       app_id: `${process.env.ONE_SIGNAL_APP_ID}`,
       included_segments: ["Subscribed Users"],
-      external_id: "string",
+
       contents: {
         en: "Test Message from Express Js",
         es: "Spanish Message",
       },
       name: "MOCCO",
-      send_after: "",
-      delayed_option: "",
-      delivery_time_of_day: "",
-      throttle_rate_per_minute: 0,
-    //   data: { postIndex: postIndex },
+      //   send_after: "",
+      //   delayed_option: "",
+      //   delivery_time_of_day: "",
+      //   throttle_rate_per_minute: 0,
+      data: { postIndex: postIndex },
     },
   };
 

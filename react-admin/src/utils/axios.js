d26941544b82426e890799f7d7bfbd210f1f8ea5
Aabid Ismail <aabidismail4@gmail.com>
2023-07-18 13:37:43 +0530

    changed from localhost to production server

--- react-admin/src/utils/axios.js
+++ react-admin/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://mocco.onrender.com",
-  baseURL: "http://localhost:5555",
+  baseURL: "https://mocco.onrender.com",
+  // baseURL: "http://localhost:5555",
 });
 
 export default instance;

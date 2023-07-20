12e36a994bdef85d9515457969774d201dfc5936
Aabid Ismail <aabidismail4@gmail.com>
2023-07-20 14:14:04 +0530

    right sidebar hidden as its under dev

--- mocco.lk/src/components/right/RightSidebar.jsx
+++ mocco.lk/src/components/right/RightSidebar.jsx
@@ -6,13 +6,13 @@ import DateString from "./DateString";
 function RightSidebar() {
   return (
     <div>
-      <DateString />
+      {/* <DateString />
       <hr></hr>
       <TodayEvents />
       <h4>Today's Quote</h4>
       <hr></hr>
       <TodayQuote />
-      <h4>Today's Sponsor</h4>
+      <h4>Today's Sponsor</h4> */}
     </div>
   );
 }
